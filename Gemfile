4c2f28293e5d2b4470ee9423e14d6ea4590007da
Michael Schinis <mschinis@gmail.com>
2022-07-25 20:46:59 +0300

    feat: Install plugins

--- Gemfile
+++ Gemfile
@@ -3,5 +3,5 @@ source 'https://rubygems.org'
 
 gem 'jekyll', '~> 4.2.1'
 gem 'jekyll-redirect-from'
-
+gem 'jekyll-environment-variables'
 gem "webrick", "~> 1.7"
---
+++ _plugins/somesite_env.rb
@@ -0,0 +1,10 @@
+# module Jekyll
+#     class EnvironmentVariablesGenerator < Generator
+#         def generate(site)
+#             if ENV['SITE'] != ""
+#                 site.config['somesite'] = ENV['SITE']
+#             end
+#         # Add other environment variables to `site.config` here...
+#         end
+#     end
+# end
